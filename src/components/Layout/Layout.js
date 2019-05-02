14851b428bd8460fdd2dcc4f74d0c8e4f7d551cd
Zachary Sohovich <zachsohovich@thebriga.de>
2019-05-02 16:12:56 -0700

    added proper styling per comps

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,40 +1,38 @@
 import React, { Fragment } from "react"
-import { Helmet } from "react-helmet"
+import styled from "@emotion/styled"
 
-import { rhythm } from "../../utils/typography"
 import Navigation from "./Navigation"
+import Footer from "./Footer"
 
 const Layout = ({ children }) => (
   <Fragment>
-    <Helmet>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/modern-normalize/0.5.0/modern-normalize.min.css"
-      />
-    </Helmet>
     <Navigation
       navigationListItems={[
         { title: "Tutorials", href: "/tutorials" },
+        { title: "Tools / Libraries / Documentation", href: "/docs" },
         { title: "Community", href: "/community" },
-        { title: "Documetation", href: "/docs" },
       ]}
     />
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}
-    >
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
-    </div>
+    <Container>
+      <Main>{children}</Main>
+    </Container>
+    <Footer>Footer</Footer>
   </Fragment>
 )
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+const Main = styled.main`
+  max-width: 1200px;
+  width: 100%;
+  display: flex;
+  justify-content: left;
+`
 
 export default Layout
