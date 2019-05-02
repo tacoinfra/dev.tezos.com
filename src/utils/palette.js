028f917abc6d6781d5fb0fbc634f9d3fef39cbcf
Zachary Sohovich <zachsohovich@thebriga.de>
2019-05-02 16:14:24 -0700

    added new palette resource file

---
+++ src/utils/palette.js
@@ -0,0 +1,11 @@
+// NOTE: This is a palette variable intended to be use with emotion package
+const palette = {
+  white: '#FFFFFF',
+  grey: '#F6F8FD',
+  black: '#000000',
+  darkBlue: '#123262',
+  darkerBlue: '#1A2734',
+  blue: '#3380EF'
+}
+
+export default palette;
