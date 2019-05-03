44d8f80a1705c93f43b95825a62473ec278717ea
Zachary Sohovich <zachsohovich@thebriga.de>
2019-05-03 13:31:47 -0700

    added posts json file for holding posts data

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,16 @@ import React from "react"
 import { graphql } from "gatsby"
 import SEO from "../components/SEO"
 import { Layout } from "../components/Layout"
+import posts from "./posts.json"
 
 const SiteIndex = ({ location }) => {
-
   // NOTE: This is the actual presentation of our data
   return (
     <Layout location={location}>
       <SEO title="Home" />
       <h1>Tezos Developer Portal</h1>
       <p>Software Developers and Blockchain developers resource</p>
+      {posts.map(post => post.title)}
     </Layout>
   )
 }
