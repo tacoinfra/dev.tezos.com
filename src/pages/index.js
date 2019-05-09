3b1d3f37925f9183c814a27c50776f10079c5033
Zachary Sohovich <zachsohovich@thebriga.de>
2019-05-09 13:05:19 -0700

    added community section

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,42 @@ const SiteIndex = ({ location }) => {
             </PostList>
           </PostColumn>
         </PostsSection>
-        <CommunitySection />
+        <CommunitySection>
+          <TetrisBlock />
+          <h3>Dev Support Channels</h3>
+          <CommunitiesList>
+            <li>
+              <StackExchangeLogo />
+              {/* NOTE: Icon has a little bit of margin in it, so we correct the header's spacing to accomodate */}
+              {/* TODO: Actually correct the svg and remove this inline styling */}
+              <h3
+                style={{
+                  marginLeft: "4",
+                }}
+              >
+                StackExchange
+              </h3>
+            </li>
+            <li>
+              <RedditLogo />
+              <h3>Reddit</h3>
+            </li>
+            <li>
+              <RiotLogo />
+              <h3>Riot</h3>
+            </li>
+          </CommunitiesList>
+        </CommunitySection>
+        <MailingListSection>
+          <MailIcon />
+          <h3>Developer Mailing List</h3>
+          <form action="" method="POST">
+            <StyledInput type="email" placeholder="Email" />
+            <ButtonAnchor isSmall isAnchor={false} type="submit">
+              Submit
+            </ButtonAnchor>
+          </form>
+        </MailingListSection>
       </Wrapper>
     </Layout>
   )
@@ -125,6 +160,78 @@ const CommunitySection = styled.div`
   height: 275px;
   background-color: ${palette.lighterGrey};
   width: 100%;
+  h3 {
+    margin-bottom: 0;
+    margin-left: 18px;
+  }
+`
+
+const CommunitiesList = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  list-style-type: none;
+  margin: 0;
+  &::before {
+    content: "";
+    display: block;
+    height: 133px;
+    background-color: ${palette.grey};
+    width: 1px;
+    margin: 0 31.5px;
+  }
+  li {
+    margin-bottom: 0;
+    margin-right: 21px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: center;
+    align-items: center;
+    svg {
+      height: 58px;
+      width: 58px;
+    }
+  }
+`
+
+// MAILINGLIST Components and Styles
+const MailingListSection = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 130px 0;
+  > * {
+    margin-bottom: 0;
+    margin-right: 30px;
+  }
+  form {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: nowrap;
+    button {
+      margin-left: 41px;
+    }
+  }
+`
+
+const StyledInput = styled.input`
+  padding: 0px 0px 9px;
+  background: rgba(0, 0, 0, 0) none repeat scroll 0% 0%;
+  border-color: ${palette.gray};
+  border-style: none none solid;
+  border-width: medium medium 1px;
+  border-image: none 100% / 1 / 0 stretch;
+  border-radius: 0px;
+  color: rgb(0, 0, 0);
+  font-size: 16px;
+  line-height: 24px;
+  width: 100%;
+  -moz-appearance: none;
+  margin-bottom: 0px;
+  display: block;
+  width: 310px;
 `
 
 export default SiteIndex
