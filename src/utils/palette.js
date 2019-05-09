c78f822ebad0f264caa54db58d29f802c0546607
Zachary Sohovich <zachsohovich@thebriga.de>
2019-05-09 09:32:47 -0700

    added more colors tp palette

--- src/utils/palette.js
+++ src/utils/palette.js
@@ -1,7 +1,9 @@
 // NOTE: This is a palette variable intended to be use with emotion package
 const palette = {
   white: '#FFFFFF',
-  grey: '#F6F8FD',
+  lighterGrey: '#F0F3F9',
+  lightGrey: '#F6F8FD',
+  grey: '#DCE3F0',
   black: '#000000',
   darkBlue: '#123262',
   darkerBlue: '#1A2734',
