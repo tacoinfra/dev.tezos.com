c63882c2a8c7afbca608831c860b066c34f22803
Zachary Sohovich <zachsohovich@thebriga.de>
2019-05-09 09:31:47 -0700

    updated layout to use Hero component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,10 +2,12 @@ import React from "react"
 import styled from "@emotion/styled"
 import { Global, css } from "@emotion/core"
 import Header from "./Header"
+import CTA from "./CTA"
+import Hero from "../components/Hero"
 import Footer from "./Footer"
 import palette from "../utils/palette"
 
-const Layout = ({ children }) => {
+const Layout = ({ children, location, title, subtitle }) => {
   const globalRules = css`
     h1 {
       color: ${palette.darkBlue};
@@ -19,7 +21,22 @@ const Layout = ({ children }) => {
     <Container>
       <Global styles={globalRules} />
       <Header />
-      <Main>{children}</Main>
+      <Hero title={title} subtitle={subtitle}>
+        {location.pathname === "/" && (
+          <div css={css`
+            display: flex;
+            justify-content: space-evenly;
+            width: 552px;
+            a + a {
+              margin-left: 40px;
+            }
+          `}>
+            <CTA href="#">Build a Node</CTA>
+            <CTA href="#">Run a Node</CTA>
+          </div>
+        )}
+      </Hero>
+      <Main isHome={location.pathname === "/"}>{children}</Main>
       <Footer />
     </Container>
   )
@@ -31,13 +48,14 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
+  background-color: ${palette.white};
 `
 
 const Main = styled.main`
   width: 100%;
-  max-width: 1200px;
   min-height: calc(100vh - 452px - 90px); /* 100 view height - size of footer (452px) - size of header (90px) */
-  margin: 148px 0;
+  transform: ${props => props.isHome ? "translateY(0)" : "translateY(-80px)"};
+  background-color: ${palette.white};
 `
 
 export default Layout
