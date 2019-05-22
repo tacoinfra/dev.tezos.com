695f5c2b8f6dee686059c908d0c34a5248f6979f
Eevee Kreevee <thekrambuhl@gmail.com>
2019-05-22 16:19:40 -0700

    move map/reduce out of render function, doesn't rely on props and could grow less computationally friendly

--- src/pages/tutorials.js
+++ src/pages/tutorials.js
@@ -6,34 +6,37 @@ import CollapsibleItem from "../components/CollapsibleItem"
 import { palette, breakpoints, spacings } from "../utils/variables"
 import contentList from "./content.json"
 
+// First we organize all content with category of tutorial
+const tutorials = contentList.filter(
+  content => content.category === "tutorial"
+)
+
+// Second we grab an array of all the different subcategories
+const subcategories = tutorials
+  .map(tutorial => tutorial.subcategory) // First we map out each subcategory
+  .reduce((accumulator, currentValue) => {
+    // Next we call reduce to remove duplicates
+    if (accumulator.indexOf(currentValue) === -1) {
+      // First we check if the current subcategory already exists in our array
+      return [...accumulator, currentValue] // If it doesn't, add it
+    } else {
+      return accumulator // Otherwise just skip it (by returning the accumulator as is)
+    }
+  }, [])
+
+// Last we created an object that has each subcategory as a key, and the respective tutorials for that subcategory as their values
+const organizedTutorials = subcategories.reduce(
+  (accumulator, currentValue) => ({
+    ...accumulator, // We return an object that spreads our current accumulator
+    [currentValue]: tutorials.filter(
+      // We set the currentValue as a key and grab all the tutorials with that subcategory (currentValue)
+      tutorial => tutorial.subcategory === currentValue
+    ),
+  }),
+  {}
+)
+
 const TutorialIndex = ({ location }) => {
-  // First we organize all content with category of tutorial
-  const tutorials = contentList.filter(
-    content => content.category === "tutorial"
-  )
-  // Second we grab an array of all the different subcategories
-  const subcategories = tutorials
-    .map(tutorial => tutorial.subcategory) // First we map out each subcategory
-    .reduce((accumulator, currentValue) => {
-      // Next we call reduce to remove duplicates
-      if (accumulator.indexOf(currentValue) === -1) {
-        // First we check if the current subcategory already exists in our array
-        return [...accumulator, currentValue] // If it doesn't, add it
-      } else {
-        return accumulator // Otherwise just skip it (by returning the accumulator as is)
-      }
-    }, [])
-  // Last we created an object that has each subcategory as a key, and the respective tutorials for that subcategory as their values
-  const organizedTutorials = subcategories.reduce(
-    (accumulator, currentValue) => ({
-      ...accumulator, // We return an object that spreads our current accumulator
-      [currentValue]: tutorials.filter(
-        // We set the currentValue as a key and grab all the tutorials with that subcategory (currentValue)
-        tutorial => tutorial.subcategory === currentValue
-      ),
-    }),
-    {}
-  )
   return (
     <Layout location={location} title="Tutorials" compact>
       <SEO title="All posts" />
