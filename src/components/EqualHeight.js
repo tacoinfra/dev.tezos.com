71b537a86073342ae6960dfb052d5e758b0136e8
Eevee Kreevee <thekrambuhl@gmail.com>
2019-05-28 15:16:19 -0700

    update equal height code to match height when items are at the same top offset

--- src/components/EqualHeight.js
+++ src/components/EqualHeight.js
@@ -4,22 +4,35 @@ import debounce from "lodash.debounce"
 const EqualHeight = ({ nodes, children }) => {
   const handleResize = debounce(() => {
     const currentNodes = nodes.map(node => node.current).filter(a => a)
-    const heightList = currentNodes.map(node => {
+    const dimensionsList = currentNodes.map(node => {
       // remove any forced sizing information
       node.style.height = 'auto'
       node.style.maxHeight = ''
       node.style.minHeight = ''
 
       // get natural size
-      const { height } = node.getBoundingClientRect()
-
-      return height
+      const { top, height } = node.getBoundingClientRect()
+      return { node, top, height }
     })
-    const maxHeight = Math.max(...heightList)
 
-    // update height using the max height
-    currentNodes.forEach(node => {
-      node.style.height = `${maxHeight}px`;
+    // group nodes by top offset
+    const nodeGroups = Object.values(
+      dimensionsList.reduce((accumulator, { node, top, height }) => {
+        if (!accumulator[top]) accumulator[top] = []
+        accumulator[top].push({ node, height })
+        return accumulator
+      }, { })
+    )
+
+    // get max height and update height for each group
+    nodeGroups.forEach((group) => {
+      const heightList = group.map(item => item.height)
+      const maxHeight = Math.max(...heightList)
+
+      // update height using the max height
+      group.forEach(({ node }) => {
+        node.style.height = `${maxHeight}px`;
+      })
     })
   }, 50)
 
