a1beb5a99a3dc951e839cb5fc7b7e326c4839e86
Eevee Kreevee <thekrambuhl@gmail.com>
2019-05-29 13:33:25 -0700

    add `dev.tezos.com` pathPrefix to gatsby config temporarily
    - TODO: when DNS has been setup, this should be removed and redeployed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 const languages = require("./src/utils/languages.js")
 
 module.exports = {
+  pathPrefix: '/dev.tezos.com',
   siteMetadata: {
     title: `Tezos Developer Portal`,
     author: `Tezos`,
