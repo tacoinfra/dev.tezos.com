d2491149e1a58adb83330a4e01177bf56d349a76
Eevee Kreevee <thekrambuhl@gmail.com>
2019-06-19 12:18:48 -0700

    fix linting errors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from "@emotion/styled"
 import CircleIcon from "./CircleIcon"
-import { palette, breakpoints, spacings } from "../utils/variables"
+import { palette } from "../utils/variables"
 
 // images
 import redditIcon from "../assets/icon-reddit.png"
