52a2b1df8a91e1212e5eb61373392c9df6b4d5d3
Eevee Kreevee <thekrambuhl@gmail.com>
2019-06-25 13:18:44 -0700

    fix head/meta elements

--- src/components/seo.js
+++ src/components/seo.js
@@ -29,56 +29,30 @@ function SEO({ description, lang, meta, keywords, title }) {
   const siteTitle = title ? `${title} | ${site.siteMetadata.title}` : `${site.siteMetadata.title}`
 
   return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={siteTitle}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: siteTitle,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: siteTitle,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ]
-        .concat(
-          keywords.length > 0
-            ? {
-                name: `keywords`,
-                content: keywords.join(`, `),
-              }
-            : []
-        )
-        .concat(meta)}
-    >
+    <Helmet>
+      <html lang={lang || 'en'} />
+      <title>{siteTitle}</title>
+
+      {/* dont index while in beta */}
       <meta name="robots" content="noindex" />
+
+      <meta name="author" content={site.siteMetadata.author} />
+      <meta name="description" content={metaDescription} />
+      {
+        keywords.length > 0 &&
+        <meta name="keywords" content={keywords.join(`, `)} />
+      }
+
+      {/* og tags */}
+      <meta name="og:title" content={siteTitle} />
+      <meta name="og:description" content={metaDescription} />
+      <meta name="og:type" content="website" />
+
+      {/* twitter tags */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:creator" content={site.siteMetadata.author} />
+      <meta name="twitter:title" content={siteTitle} />
+      <meta name="twitter:description" content={metaDescription} />
     </Helmet>
   )
 }
