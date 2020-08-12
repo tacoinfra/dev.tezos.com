133f35a2d7a0aa31a1bec9e9740c4bbc39d59859
Igor Tkach <igor@tqgroup.io>
2020-08-12 15:33:46 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e0483548e95f4e9324fb.js"
+    "url": "webpack-runtime-fadfd5f7d1cae81312ab.js"
   },
   {
     "url": "styles.1b1b71b2f05e6df792a5.css"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0d7170de8d8f34b0a03f981c39edbacc"
+    "revision": "82644325c8cd48ae826a10c75630b934"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-f4d8f14b77ccd5568123.js"
